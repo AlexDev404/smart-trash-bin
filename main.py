a9ef97891ca56a9613e4a7692386b36275a5b65e
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2025-11-24 17:57:37 -0600

    add buzzer code

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Requires the micropython-servo library - https://pypi.org/project/micropython-se
 import time
 from servo import Servo
 from hcsr04 import HCSR04
-from machine import Pin
+from machine import Pin, PWM
 
 # Create our Servo object, assigning the
 # GPIO pin connected the PWM wire of the servo
@@ -15,8 +15,18 @@ TIMEOUT_S = 3  # Timeout duration in seconds
 my_servo = Servo(pin_id=16)
 trigger_pin = Pin(14, Pin.OUT)
 echo_pin = Pin(15, Pin.IN)
-sensor = HCSR04(trigger_pin=trigger_pin, echo_pin=echo_pin, echo_timeout_us=1000000)
+sensor = HCSR04(trigger_pin=trigger_pin, echo_pin=echo_pin, echo_timeout_us=100000)
+buzzer_pin = Pin(0, Pin.OUT)
+buzzer_pwm = PWM(buzzer_pin)
 wants_time_out = False
+
+
+def buzzer_play(hz, duration_ms):
+    buzzer_pwm.freq(hz)
+    buzzer_pwm.duty_u16(32768)  # 50% duty cycle
+    time.sleep_ms(duration_ms)
+    buzzer_pwm.duty_u16(0)  # Turn off the buzzer
+
 
 while True:
     distance = sensor.distance_cm()
@@ -29,6 +39,7 @@ while True:
         wants_time_out = True
     else:
         if wants_time_out:
+            # buzzer_play(1000, 200)
             print("TIMEOUT (3s)")
             time.sleep(TIMEOUT_S)
             wants_time_out = False
